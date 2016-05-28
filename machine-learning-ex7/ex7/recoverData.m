3d310a1e43180e885b837458ad19cfa72d854e03
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-05-28 19:50:45 +0800

    Finish recovering data

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -20,7 +20,11 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %
 %               Notice that U(j, 1:K) is a row vector.
 %               
-
+m = size(Z,1);
+for (i = 1:m)
+v = Z(i,:)';
+X_rec(i,:) = v'*U(:,1:K)';
+end
 
 
 % =============================================================
